dfd687fbd7ffca82d37ebd332a5f39946a491fcd
john-molenda1 <92773195+john-molenda1@users.noreply.github.com>
2022-02-05 22:44:18 -0500

    Deliverable 2 Commit
    Created summary tables to describe the Suspension Coil dataset to determine the mean, median, variance, and standard deviation of the entire dataset. Then I performed the same analysis grouped by manufacturing lots.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,6 @@ library("dplyr")
 mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table)) #generate summary statistics
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # create a total summary table 
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table for manufacturing lots
